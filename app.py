5bedbe9142d3341b78316df6a31c70cacf5c756f
Biomara Valenzuela Godina <biomara76@gmail.com>
2025-05-18 21:15:02 -0600

    github con azure

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def inicio():
+    return """
+    <h1>¡Hola desde Azure!</h1>
+    <p>Nombre completo: "Biomara Valenzuela Godina"</p>
+    <p>Carrera: "mi carrera es ingenieria informatica"</p>
+    <p>Semestre: "estoy en octavo semestre"</p>
+    <p>No. de Control: 21550748 </p>
+    """
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8000)
